dd52c0dfc14092906c7a4ab5997530b6bbe3004d
Dipak kumar mali <dk2178mali@gmail.com>
2022-07-27 09:55:35 +0530

    code for updation or patch request is completed

--- controller/user/user-cont.js
+++ controller/user/user-cont.js
@@ -1,45 +1,65 @@
-const user = require('../../model/user/user-schema')
+const user = require("../../model/user/user-schema");
 const Meal = require("../../model/meal/meal-schema");
- const {createUser} = require('../../model/user/user-model')
+const { createUser,updateUser } = require("../../model/user/user-model");
+const User = require("../../model/user/user-schema");
 module.exports = {
-    // create get Users object for get all user data
-    get_user :async (req,res) =>
-    {
-            //const result = await user.find() ;
-            user.find().populate("mealPlan.meals")
-                .then(result => {                  
-                   res.json(result)
-                })
-                .catch(error => {
-                    console.log(error)
-                    res.status(500).json({ message: error.msg });
-                });
-       
-    },
+  // create get Users object for get all user data
+  get_user: (req, res) => {
+    user
+      .findOne({ name: "dipak" })
+      .populate({
+        path: "mealPlan.meals",
+        populate: { path: "foodItem" },
+      })
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ message: error.msg });
+      });
+  },
 
-    create_user :async (req,res) =>
-    {
-        try {
-            const data = req.body;
-            createUser(data, (err, result) => {
-                if (err) {
-                  console.log(err);
-                  res.status(500).json({
-                    success: 0,
-                    message: "Data may be not valid or connectivity error",
-                  });
-                }else{
-                return res.status(200).json({
-                    success : 1,
-                    data : result
-                })
-              }
-              })
-        } catch (error) {
-            console.log(error.msg)
+  create_user: async (req, res) => {
+    try {
+      const data = req.body;
+      createUser(data, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            success: 0,
+            message: "Data may be not valid or connectivity error",
+          });
+        } else {
+          return res.status(200).json({
+            success: 1,
+            data: result,
+          });
         }
-    },
-    update_user : async() => {
-        console.log(error.msg)
+      });
+    } catch (error) {
+      console.log(error.msg);
     }
-}
+  },
+  update_user: async (req,res) => {
+   // const findOneAndUpdate()
+   try{
+   const body = req.body;
+   updateUser(body,(result) => {    
+    if (result !== null) {
+      return res.status(200).json({
+        success: 1,
+        data: "Data updation successfully! ",
+      });
+    } else {
+      return res.json({
+        success: 0,
+        message: "Data/ Record Not found or Invalid",
+      });
+    }
+  })
+    }catch(error){
+        console.log(error);
+    }
+  },
+};
--- model/user/user-model.js
+++ model/user/user-model.js
@@ -2,16 +2,28 @@ const User = require("./user-schema");
 
 module.exports = {
   createUser: async (data, cb) => {
-    console.log(data.mealPlan);
+    //console.log(data.mealPlan);
     try {
       const document = new User({
         name: data.name,
         calorieRequirement: data.calorieRequirement,
-        mealPlan:data.mealPlan
+        mealPlan: data.mealPlan,
       });
       const result = await document.save();
       //console.log(result);
       return cb(null, result);
+    } catch (err) {
+      console.log(err);
+      return cb(err);
+    }
+  },
+  updateUser: async (data, cb) => {
+    try {
+      const filter = { name: data.name };
+      const update = data;
+      const result = await User.findOneAndUpdate(filter, update);
+      //console.log(result)
+      return cb(result);
     } catch (err) {
       console.log(err);
       return cb(err);
